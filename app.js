1ce1f52540e5f469da0a49455e31c6dd989f94e1
chaim cymerman <a548492309@gmail.com>
2023-08-17 16:33:44 +0300

    first commit

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-import express from 'express';
-import userRouter from './users.routes.js';
+// import express from 'express';
+// import userRouter from './users.routes.js';
+
+// const app = express();
+
+// app.use(express.json());
+
+// const port = 8080;
+
+// app.use('/', userRouter);
+
+// app.listen(port, () => {
+//     console.log(`Server is up and running on port: ${port}`);
+// });
+
+
+
+import express from "express";
+import usersRoute from './user.repostory.js'
 
 const app = express();
+const port = 3000;
 
 app.use(express.json());
+app.use("/users", usersRoute)
 
-const port = 8080;
-
-app.use('/', userRouter);
 
 app.listen(port, () => {
-    console.log(`Server is up and running on port: ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
+
+
--- users.router.js
+++ users.router.js
@@ -1,19 +1,35 @@
-// users.routes.js
-import express from 'express';
-import usersService from './users.services.js';
+// // users.routes.js
+// import express from 'express';
+// import usersService from './users.services.js';
 
 
 
 
+
+// const router = express.Router();
+
+// router.post('/register', usersService.register);
+
+
+
+
+
+// export default router;
+
+
+import express from "express";
+import userController from "../users.controler.js"
 
 const router = express.Router();
 
-router.post('/register', usersService.register);
+router.get("/", userController.getAllUsers)
 
+router.get("/:id", userController.getUser)
 
+router.post("/", userController.createUser);
 
+router.put("/:id", userController.updateUser)
 
+router.delete("/:id", userController.deleteUser)
 
-export default router;
-
-
+export default router 
